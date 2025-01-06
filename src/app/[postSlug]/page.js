e3a3ffb85bd4b5b6bc7a78ce27439f69ebdf358b
armornick <nickstories@gmail.com>
2025-01-06 09:23:16 +0100

    use file helper instead of custom function

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,19 +1,9 @@
-import { readFile } from "node:fs/promises";
-import { join } from "node:path";
 import React from "react";
 import { MDXRemote } from "next-mdx-remote/rsc";
-
+import { loadBlogPost } from "@/helpers/file-helpers";
 import BlogHero from "@/components/BlogHero";
 
 import styles from "./postSlug.module.css";
-import matter from "gray-matter";
-
-async function loadBlogPost(slug) {
-	const filePath = join(process.cwd(), "content", slug + ".mdx");
-	const rawFile = await readFile(filePath, "utf-8");
-	const fileData = matter(rawFile);
-	return fileData;
-}
 
 async function BlogPost({ params }) {
 	const { postSlug } = await params;
@@ -22,8 +12,8 @@ async function BlogPost({ params }) {
 	return (
 		<article className={styles.wrapper}>
 			<BlogHero
-				title={blogPost.data.title}
-				publishedOn={blogPost.data.publishedOn}
+				title={blogPost.frontmatter.title}
+				publishedOn={blogPost.frontmatter.publishedOn}
 			/>
 			<div className={styles.page}>
 				<MDXRemote source={blogPost.content} />
