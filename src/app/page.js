40d4d0aa3624bf185aa1f1efcd352420bac9a914
armornick <nickstories@gmail.com>
2025-01-06 09:09:54 +0100

    Exercise 1: Homepage list of posts

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,49 @@
-import React from 'react';
+import { readdir, readFile } from "node:fs/promises";
+import { join, basename, extname } from "node:path";
+import React from "react";
+import matter from "gray-matter";
 
-import BlogSummaryCard from '@/components/BlogSummaryCard';
+import BlogSummaryCard from "@/components/BlogSummaryCard";
 
-import styles from './homepage.module.css';
+import styles from "./homepage.module.css";
 
-function Home() {
-  return (
-    <div className={styles.wrapper}>
-      <h1 className={styles.mainHeading}>
-        Latest Content:
-      </h1>
+async function getBlogPosts() {
+	const result = [];
+	const files = await readdir(join(process.cwd(), "/content"));
+	for (const file of files) {
+		const rawFile = await readFile(
+			join(process.cwd(), "content", file),
+			"utf-8"
+		);
+		const { data: frontmatter } = matter(rawFile);
+		result.push({
+			slug: basename(file, extname(file)),
+			...frontmatter,
+			publishedOn: new Date(frontmatter.publishedOn),
+		});
+	}
+	result.sort((a, b) => b.publishedOn.getTime() - a.publishedOn.getTime());
+	return result;
+}
 
-      {/* TODO: Iterate over the data read from the file system! */}
-      <BlogSummaryCard
-        slug="example"
-        title="Hello world!"
-        abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
-        publishedOn={new Date()}
-      />
-    </div>
-  );
+async function Home() {
+	const blogPosts = await getBlogPosts();
+
+	return (
+		<div className={styles.wrapper}>
+			<h1 className={styles.mainHeading}>Latest Content:</h1>
+
+			{blogPosts.map((blogPost) => (
+				<BlogSummaryCard
+					key={blogPost.slug}
+					slug={blogPost.slug}
+					title={blogPost.title}
+					abstract={blogPost.abstract}
+					publishedOn={blogPost.publishedOn}
+				/>
+			))}
+		</div>
+	);
 }
 
 export default Home;
